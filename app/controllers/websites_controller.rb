ab412fb40376326948f8d7bb0a26db2b204f0203
ParitoshPraharaj <parirakesh@gmail.com>
2014-04-13 22:47:42 +0530

    Added a Model for Website.

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -3,8 +3,26 @@ class WebsitesController < ApplicationController
   end
 
   def lets_get_started
+	
   end
 
   def create
+	@website = Website.create(parameters_for_saving_a_website_from_a_user)
+	
+	if @website.save
+		redirect_to lets_begin_work_path
+	else
+		render lets_get_started
+	end	
+	
   end
+  
+  def lets_begin_work
+  
+  end
+  
+  def parameters_for_saving_a_website_from_a_user
+	params.require(:website).permit(:people_name, :people_email, :website_name, :website_description)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ TheDesignStudio::Application.routes.draw do
   
   resources :work
   
+  resources :websites
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
