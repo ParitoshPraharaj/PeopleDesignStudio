c55713c61d98fb2979c9fa2de3977718092ad7a6
ParitoshPraharaj <parirakesh@gmail.com>
2014-04-16 08:08:09 +0530

    Added Sign In Action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def sign_in
+    people = People.where(email: params[:email]).first
+    if people && people.authenticate(params[:password])
+      session[:people_id] = people.id
+      redirect_to projects_path, notice: "Hey, Welcome Back Gain. Let's Get Started With Work."
   end
 
   def sign_out
