475300f04472a68353fd8fafa866491fc42610bd
ParitoshPraharaj <parirakesh@gmail.com>
2014-04-17 16:27:40 +0530

    Added Validations to The Website Model

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -4,7 +4,7 @@ class WebsitesController < ApplicationController
   end
 
   def index
-      @websites = Website.all
+      @websites = Website.order(created_at: :desc).limit(3)
   end
 
   def lets_get_started
@@ -13,22 +13,27 @@ class WebsitesController < ApplicationController
 
   def create
 	@website = Website.create(parameters_for_saving_the_website_form)
-        logger.info "A Website Saved: @website.atrributes.inspect"
+        logger.info "A Website Saved: @website.attributes.inspect"
 	if @website.save
-	    redirect_to lets_begin_work_path(@website)
+		@person_name = @website.people_name
+	    redirect_to lets_begin_work_path(@person_name)		
 	else
-            logger.info "Attributes did not got saved: @website.attributes.inspect"
-            render lets_get_started
+        logger.info "Attributes did not got saved: @website.attributes.inspect"
+        render lets_get_started
 	end	
-	
   end
   
   def lets_begin_work
-	
+	#@person_just_created_a_project = website.people_name
+	#render lets_begin_work(@person_just_created_a_project)
   end
   
   def parameters_for_saving_the_website_form
 	params.require(:website).permit(:people_name, :people_email, :website_name, :website_description)
   end
+  
+  
+  
+  
   
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,2 +1,3 @@
 class Website < ActiveRecord::Base
+	validates :people_name, :people_email, :website_name, :website_description, presence: true
 end
