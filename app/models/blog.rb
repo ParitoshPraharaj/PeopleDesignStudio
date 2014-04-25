526ba4c6f681052497d3f63f2d5088f2d50cdd7f
ParitoshPraharaj <parirakesh@gmail.com>
2014-04-25 05:45:27 +0530

    Made the Note Show Action to Unescape HTML in the Views?:

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ActiveRecord::Base
+		
+	def better_title
+		title.downcase.gsub(" ", "-")
+	end
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,9 @@
 class Note < ActiveRecord::Base
+		
+	def titles_in_urls
+		title.downcase.gsub(" ","-")
+	end
+	
+	
+	
 end
